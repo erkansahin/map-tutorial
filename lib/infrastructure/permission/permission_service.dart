0fcbf88ac6eec18a82552da6490ca12ef27acb57
aytunc <aytunch@gmail.com>
2022-10-30 19:28:57 +0300

    permission infrastructure added

---
+++ lib/infrastructure/permission/permission_service.dart
@@ -0,0 +1,40 @@
+import 'package:geolocator/geolocator.dart';
+import 'package:injectable/injectable.dart';
+import 'package:map_tutorial_template/domain/permission/location_permission_status.dart';
+
+import '../../domain/permission/i_permission_service.dart';
+
+@LazySingleton(as: IPermissionService)
+class PermissionService implements IPermissionService {
+  @override
+  Stream<bool> get locationServicesStatusStream =>
+      Geolocator.getServiceStatusStream()
+          .map((serviceStatus) => serviceStatus == ServiceStatus.enabled);
+
+  @override
+  Future<bool> isLocationPermissionGranted() async {
+    final status = await Geolocator.checkPermission();
+    final isGranted = status == LocationPermission.always ||
+        status == LocationPermission.whileInUse;
+
+    return isGranted;
+  }
+
+  @override
+  Future<bool> isLocationServicesEnabled() {
+    return Geolocator.isLocationServiceEnabled();
+  }
+
+  @override
+  Future<LocationPermissionStatus> requestLocationPermission() async {
+    final status = await Geolocator.requestPermission();
+    LocationPermissionStatus result = LocationPermissionStatus.granted;
+    if (status == LocationPermission.deniedForever) {
+      result = LocationPermissionStatus.deniedForever;
+    } else if (status == LocationPermission.denied ||
+        status == LocationPermission.unableToDetermine) {
+      result = LocationPermissionStatus.denied;
+    }
+    return result;
+  }
+}
--- lib/injection.config.dart
+++ lib/injection.config.dart
@@ -8,8 +8,9 @@
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
-import 'application/permission/permission_cubit.dart' as _i3;
-import 'domain/permission/i_permission_service.dart'
+import 'application/permission/permission_cubit.dart' as _i5;
+import 'domain/permission/i_permission_service.dart' as _i3;
+import 'infrastructure/permission/permission_service.dart'
     as _i4; // ignore_for_file: unnecessary_lambdas
 
 // ignore_for_file: lines_longer_than_80_chars
@@ -24,7 +25,8 @@ _i1.GetIt $initGetIt(
     environment,
     environmentFilter,
   );
-  gh.lazySingleton<_i3.PermissionCubit>(
-      () => _i3.PermissionCubit(get<_i4.IPermissionService>()));
+  gh.lazySingleton<_i3.IPermissionService>(() => _i4.PermissionService());
+  gh.lazySingleton<_i5.PermissionCubit>(
+      () => _i5.PermissionCubit(get<_i3.IPermissionService>()));
   return get;
 }
