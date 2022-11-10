26b55a66855eb250a6363e448e3ed68a1f09dff4
Erkan Sahin <36175542+erkansahin@users.noreply.github.com>
2022-11-11 02:14:58 +0300

    bloc-bloc communication between location cubit and applifecycle cubit is implemented

--- lib/application/location/location_cubit.dart
+++ lib/application/location/location_cubit.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:injectable/injectable.dart';
+import 'package:map_tutorial_template/application/application_life_cycle/application_life_cycle_cubit.dart';
 import 'package:map_tutorial_template/application/permission/permission_cubit.dart';
 import 'package:map_tutorial_template/domain/location/i_location_service.dart';
 import 'package:map_tutorial_template/domain/location/location_model.dart';
@@ -15,9 +16,13 @@ part 'location_state.dart';
 class LocationCubit extends Cubit<LocationState> {
   final ILocationService _locationService;
   final PermissionCubit _permissionCubit;
+  final ApplicationLifeCycleCubit _applicationLifeCycleCubit;
   StreamSubscription<LocationModel>? _userPositionSubscription;
   StreamSubscription<List<PermissionState>>? _permissionStatePairSubscription;
-  LocationCubit(this._locationService, this._permissionCubit)
+  StreamSubscription<List<ApplicationLifeCycleState>>?
+      _appLifeCycleStatePairSubscription;
+  LocationCubit(this._locationService, this._permissionCubit,
+      this._applicationLifeCycleCubit)
       : super(LocationState.initial()) {
     if (_permissionCubit.state.isLocationPermissionGrantedAndServicesEnabled) {
       _userPositionSubscription =
@@ -42,12 +47,33 @@ class LocationCubit extends Cubit<LocationState> {
         _userPositionSubscription?.cancel();
       }
     });
+
+    _appLifeCycleStatePairSubscription = _applicationLifeCycleCubit.stream
+        .startWith(_applicationLifeCycleCubit.state)
+        .pairwise()
+        .listen((pair) async {
+      final previous = pair.first;
+      final current = pair.last;
+      final isLocationPermissionGrantedAndServicesEnabled =
+          _permissionCubit.state.isLocationPermissionGrantedAndServicesEnabled;
+      if (previous.isResumed != current.isResumed &&
+          current.isResumed &&
+          isLocationPermissionGrantedAndServicesEnabled) {
+        await _userPositionSubscription?.cancel();
+        _userPositionSubscription =
+            _locationService.positionStream.listen(_userPositionListener);
+      } else if (previous.isResumed != current.isResumed &&
+          !current.isResumed) {
+        await _userPositionSubscription?.cancel();
+      }
+    });
   }
 
   @override
   Future<void> close() {
     _userPositionSubscription?.cancel();
     _permissionStatePairSubscription?.cancel();
+    _appLifeCycleStatePairSubscription?.cancel();
 
     return super.close();
   }
--- lib/presentation/map/map_page.dart
+++ lib/presentation/map/map_page.dart
@@ -1,48 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:map_tutorial_template/application/location/location_cubit.dart';
 import 'package:map_tutorial_template/application/permission/permission_cubit.dart';
+import 'package:map_tutorial_template/domain/location/location_model.dart';
+
+import '../../injection.dart';
 
 class MapPage extends StatelessWidget {
   const MapPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Map Tutorial"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            BlocSelector<PermissionCubit, PermissionState, bool>(
-              selector: (state) {
-                return state.isLocationPermissionGranted;
-              },
-              builder: (context, isLocationPermissionGranted) {
-                return Text(
-                    "Location Permission: ${isLocationPermissionGranted ? "enabled" : "disabled"}");
-              },
-            ),
-            const SizedBox(height: 20),
-            BlocSelector<PermissionCubit, PermissionState, bool>(
-              selector: (state) {
-                return state.isLocationServicesEnabled;
-              },
-              builder: (context, isLocationServicesEnabled) {
-                return Text(
-                    "Location Services: ${isLocationServicesEnabled ? "enabled" : "disabled"}");
-              },
-            ),
-            const SizedBox(height: 20),
-            OutlinedButton(
-              onPressed: () {
-                debugPrint("Location Services button Pressed!");
-                context.read<PermissionCubit>().requestLocationPermission();
-              },
-              child: const Text("Request Location Permission"),
-            ),
-          ],
+    return BlocProvider(
+      create: (context) => getIt<LocationCubit>(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Map Tutorial"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              BlocSelector<PermissionCubit, PermissionState, bool>(
+                selector: (state) {
+                  return state.isLocationPermissionGranted;
+                },
+                builder: (context, isLocationPermissionGranted) {
+                  return Text(
+                      "Location Permission: ${isLocationPermissionGranted ? "enabled" : "disabled"}");
+                },
+              ),
+              const SizedBox(height: 20),
+              BlocSelector<PermissionCubit, PermissionState, bool>(
+                selector: (state) {
+                  return state.isLocationServicesEnabled;
+                },
+                builder: (context, isLocationServicesEnabled) {
+                  return Text(
+                      "Location Services: ${isLocationServicesEnabled ? "enabled" : "disabled"}");
+                },
+              ),
+              const SizedBox(height: 20),
+              OutlinedButton(
+                onPressed: () {
+                  debugPrint("Location Services button Pressed!");
+                  context.read<PermissionCubit>().requestLocationPermission();
+                },
+                child: const Text("Request Location Permission"),
+              ),
+              const SizedBox(height: 20),
+              BlocSelector<LocationCubit, LocationState, LocationModel>(
+                selector: (state) {
+                  return state.userLocation;
+                },
+                builder: (context, userLocation) {
+                  return Text(
+                      "Latitude: ${userLocation.latitude}  Longitude: ${userLocation.longitude}");
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
