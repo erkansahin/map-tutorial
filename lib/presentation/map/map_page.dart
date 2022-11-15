8a94578534ce4b47dacd6d1bd90a00e5aaec321e
aytunc <aytunch@gmail.com>
2022-11-16 02:00:42 +0300

    first dialog is added

--- lib/presentation/map/map_page.dart
+++ lib/presentation/map/map_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:map_tutorial_template/application/location/location_cubit.dart';
 import 'package:map_tutorial_template/application/permission/permission_cubit.dart';
@@ -13,55 +14,144 @@ class MapPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => getIt<LocationCubit>(),
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text("Map Tutorial"),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              BlocSelector<PermissionCubit, PermissionState, bool>(
-                selector: (state) {
-                  return state.isLocationPermissionGranted;
-                },
-                builder: (context, isLocationPermissionGranted) {
-                  return Text(
-                      "Location Permission: ${isLocationPermissionGranted ? "enabled" : "disabled"}");
-                },
-              ),
-              const SizedBox(height: 20),
-              BlocSelector<PermissionCubit, PermissionState, bool>(
-                selector: (state) {
-                  return state.isLocationServicesEnabled;
-                },
-                builder: (context, isLocationServicesEnabled) {
-                  return Text(
-                      "Location Services: ${isLocationServicesEnabled ? "enabled" : "disabled"}");
-                },
-              ),
-              const SizedBox(height: 20),
-              OutlinedButton(
-                onPressed: () {
-                  debugPrint("Location Services button Pressed!");
-                  context.read<PermissionCubit>().requestLocationPermission();
-                },
-                child: const Text("Request Location Permission"),
-              ),
-              const SizedBox(height: 20),
-              BlocSelector<LocationCubit, LocationState, LocationModel>(
-                selector: (state) {
-                  return state.userLocation;
-                },
-                builder: (context, userLocation) {
-                  return Text(
-                      "Latitude: ${userLocation.latitude}  Longitude: ${userLocation.longitude}");
-                },
-              ),
-            ],
+      child: BlocListener<PermissionCubit, PermissionState>(
+        listenWhen: (p, c) {
+          return p.isLocationPermissionGrantedAndServicesEnabled !=
+                  c.isLocationPermissionGrantedAndServicesEnabled &&
+              c.isLocationPermissionGrantedAndServicesEnabled;
+        },
+        listener: (context, state) {
+          Navigator.of(context).pop();
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text("Map Tutorial"),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                BlocSelector<PermissionCubit, PermissionState, bool>(
+                  selector: (state) {
+                    return state.isLocationPermissionGranted;
+                  },
+                  builder: (context, isLocationPermissionGranted) {
+                    return Text(
+                        "Location Permission: ${isLocationPermissionGranted ? "enabled" : "disabled"}");
+                  },
+                ),
+                const SizedBox(height: 20),
+                BlocSelector<PermissionCubit, PermissionState, bool>(
+                  selector: (state) {
+                    return state.isLocationServicesEnabled;
+                  },
+                  builder: (context, isLocationServicesEnabled) {
+                    return Text(
+                        "Location Services: ${isLocationServicesEnabled ? "enabled" : "disabled"}");
+                  },
+                ),
+                const SizedBox(height: 20),
+                OutlinedButton(
+                  onPressed: () {
+                    debugPrint("Location Services button Pressed!");
+                    // context.read<PermissionCubit>().requestLocationPermission();
+                    showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          final bool isLocationPermissionGranted =
+                              context.select((PermissionCubit element) =>
+                                  element.state.isLocationPermissionGranted);
+                          final bool isLocationServicesEnabled = context.select(
+                              (PermissionCubit element) =>
+                                  element.state.isLocationServicesEnabled);
+                          return AlertDialog(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15.0),
+                            ),
+                            content: PermissionDialog(
+                              isLocationPermissionGranted:
+                                  isLocationPermissionGranted,
+                              isLocationServicesEnabled:
+                                  isLocationServicesEnabled,
+                            ),
+                          );
+                        });
+                  },
+                  child: const Text("Request Location Permission"),
+                ),
+                const SizedBox(height: 20),
+                BlocSelector<LocationCubit, LocationState, LocationModel>(
+                  selector: (state) {
+                    return state.userLocation;
+                  },
+                  builder: (context, userLocation) {
+                    return Text(
+                        "Latitude: ${userLocation.latitude}  Longitude: ${userLocation.longitude}");
+                  },
+                ),
+                // PermissionDialog(),
+              ],
+            ),
           ),
         ),
       ),
+    );
+  }
+}
+
+class PermissionDialog extends StatelessWidget {
+  final bool isLocationPermissionGranted;
+  final bool isLocationServicesEnabled;
+  const PermissionDialog({
+    Key? key,
+    required this.isLocationPermissionGranted,
+    required this.isLocationServicesEnabled,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        const SizedBox(height: 10),
+        const Text(
+            "Please allow location permission and services to view your location:)"),
+        const SizedBox(height: 15),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text("Location Permission: "),
+            TextButton(
+              onPressed: isLocationPermissionGranted
+                  ? null
+                  : () {
+                      debugPrint("Location permission button pressed!");
+                      context
+                          .read<PermissionCubit>()
+                          .requestLocationPermission();
+                    },
+              child: Text(isLocationPermissionGranted ? "allowed" : "allow"),
+            ),
+          ],
+        ),
+        const SizedBox(height: 10),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text("Location Services: "),
+            TextButton(
+              onPressed: isLocationServicesEnabled
+                  ? null
+                  : () {
+                      debugPrint("Location permission button pressed!");
+                      context.read<PermissionCubit>().openLocationSettings();
+                    },
+              child: Text(isLocationServicesEnabled ? "allowed" : "allow"),
+            ),
+          ],
+        ),
+        const SizedBox(height: 10),
+      ],
     );
   }
 }
