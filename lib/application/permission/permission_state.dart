3626d4d3ccbf8d122e78636a5843205d80f44a6e
Erkan Sahin <36175542+erkansahin@users.noreply.github.com>
2022-11-22 01:35:58 +0300

    open app settings dialog is implemented

--- lib/application/permission/permission_state.dart
+++ lib/application/permission/permission_state.dart
@@ -5,10 +5,12 @@ class PermissionState with _$PermissionState {
   const factory PermissionState({
     required bool isLocationPermissionGranted,
     required bool isLocationServicesEnabled,
+    required bool displayOpenAppSettingsDialog,
   }) = _PermissionState;
   factory PermissionState.initial() => const PermissionState(
         isLocationPermissionGranted: false,
         isLocationServicesEnabled: false,
+        displayOpenAppSettingsDialog: false,
       );
   const PermissionState._();
 
