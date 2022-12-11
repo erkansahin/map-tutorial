5342b7fce8ddd7264d2c48d2408eb08bc457fb3a
aytunc <aytunch@gmail.com>
2022-12-12 01:08:21 +0300

    animte marker

--- lib/presentation/map/map_page.dart
+++ lib/presentation/map/map_page.dart
@@ -132,16 +132,58 @@ class MapPage extends StatelessWidget {
   }
 }
 
-class UserMarker extends StatelessWidget {
+class UserMarker extends StatefulWidget {
   const UserMarker({Key? key}) : super(key: key);
 
   @override
+  State<UserMarker> createState() => _UserMarkerState();
+}
+
+class _UserMarkerState extends State<UserMarker>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<double> sizeAnimation;
+
+  @override
+  void initState() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1000),
+    );
+    sizeAnimation = Tween<double>(
+      begin: 45,
+      end: 60,
+    ).animate(CurvedAnimation(
+        parent: animationController, curve: Curves.fastOutSlowIn));
+    animationController.repeat(
+      reverse: true,
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        color: Colors.black,
-        shape: BoxShape.circle,
-      ),
+    return AnimatedBuilder(
+      animation: sizeAnimation,
+      builder: (context, child) {
+        return Center(
+          child: Container(
+            width: sizeAnimation.value,
+            height: sizeAnimation.value,
+            decoration: const BoxDecoration(
+              color: Colors.black,
+              shape: BoxShape.circle,
+            ),
+            child: child,
+          ),
+        );
+      },
       child: const Icon(
         Icons.person_pin,
         color: Colors.white,
